d9414d642cafd674ed7b4498d1f9f02722876c7b
kim noel <kimnoel@kimui-MacBookPro.local>
2023-10-29 16:11:13 +0900

    setting

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const port = 3052;
+  const port = process.env.PORT;
   await app.listen(port);
   console.log(`nest is listening to ${port}`);
 }
